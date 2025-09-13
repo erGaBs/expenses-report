2355cfdf26c98bb5888751bfbf583916f8c7bab3
Gabriel Colla <101674782+erGaBs@users.noreply.github.com>
2025-09-14 00:08:07 +0200

    prova commit

--- expences-report-frontend/src/App.js
+++ expences-report-frontend/src/App.js
@@ -1,5 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
 
 function App() {
   return (
@@ -17,9 +33,13 @@ function App() {
         >
           Learn React
         </a>
+
+        <MyButton />
+       
       </header>
     </div>
   );
 }
+
 
 export default App;
