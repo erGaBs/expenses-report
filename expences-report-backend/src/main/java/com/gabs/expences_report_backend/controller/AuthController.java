a919abdbb73fac97528c05b9082b36560e39450f
Gabriel Colla <101674782+erGaBs@users.noreply.github.com>
2025-09-13 22:02:41 +0200

    aggiunte API per login e registrazione
    L'utente può registrarsi e loggarsi, alla login verrà assegnato un token jwt necessario per l'autenticazione a tutte le api successive

---
+++ expences-report-backend/src/main/java/com/gabs/expences_report_backend/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.gabs.expences_report_backend.controller;
+
+import com.gabs.expences_report_backend.entity.User;
+import com.gabs.expences_report_backend.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> request) {
+        String token = authService.login(request.get("email"), request.get("password"));
+        return ResponseEntity.ok(Map.of("token", token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Map<String, String> request) {
+        User user = authService.register(request.get("name"),request.get("surname"),request.get("email"), request.get("password"));
+        return ResponseEntity.ok(user);
+    }
+}
---
+++ expences-report-backend/src/main/java/com/gabs/expences_report_backend/service/AuthService.java
@@ -0,0 +1,44 @@
+package com.gabs.expences_report_backend.service;
+
+import com.gabs.expences_report_backend.entity.User;
+import com.gabs.expences_report_backend.repository.UserRepository;
+import com.gabs.expences_report_backend.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public String login(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return jwtUtil.generateToken(user.getEmail());
+    }
+
+    public User register(String name, String surname, String email,  String password) {
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new RuntimeException("email already exists");
+        }
+        User newUser = new User();
+        newUser.setName(email);
+        newUser.setSurname(email);
+        newUser.setEmail(email);
+        newUser.setPassword(passwordEncoder.encode(password));
+        return userRepository.save(newUser);
+    }
+}
