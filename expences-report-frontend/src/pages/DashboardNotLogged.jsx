01270ba43f9fdcaaecd1dadbbabbd975279fd171
Gabriel Colla <101674782+erGaBs@users.noreply.github.com>
2025-09-14 22:58:03 +0200

    aggiunta label per indicazione login

--- expences-report-frontend/src/pages/DashboardNotLogged.jsx
+++ expences-report-frontend/src/pages/DashboardNotLogged.jsx
@@ -5,7 +5,10 @@ import Button from "@mui/material/Button";
 export default function DashboardNotLogged() {
   return (
     <div className='d-flex flex-column justify-content-center align-items-center mt-5 text-center gap-2'>
-      <h1>Dashboard, ma non sei loggato</h1>
+      <h1>Dashboard</h1>
+      <br></br>
+      <span>Non sei loggato, effettua la login per avere accesso ai tuoi report</span>
+       <br></br>
       <NavigationButton to="/login" label="Vai al Login" />
     </div>
   );
